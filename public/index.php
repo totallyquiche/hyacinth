5ca7ce4551c3cfc221e7035d9e8f9efb02f84dbe
vagrant <vagrant@homestead>
2020-09-05 21:17:48 +0000

    Cleaning up index.php

--- public/index.php
+++ public/index.php
@@ -3,15 +3,3 @@
 declare(strict_types=1);
 
 require_once('../vendor/autoload.php');
-
-use Hyacinth\Input;
-use Hyacinth\Textarea;
-
-$break = '<br><br>';
-
-$input = new Input;
-$textarea = new Textarea;
-
-echo $input;
-echo $break;
-echo $textarea;
