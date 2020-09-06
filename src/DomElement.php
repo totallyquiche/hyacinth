809f00710823735030dfad9f913853d2d73ab794
Brian Dady <bndady@gmail.com>
2020-09-06 20:29:24 +0000

    Makes Input inherit from new abstact DomElement class

---
+++ src/DomElement.php
@@ -0,0 +1,199 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyacinth;
+
+abstract class DomElement
+{
+    /**
+     * An array of attribute name => value pairs for the element.
+     * 
+     * @var array
+     */
+    protected $attributes = [];
+
+    /**
+     * The name of the tag.
+     * 
+     * @var string
+     */
+    protected $tag_name;
+
+    /**
+     * Instantiate the object. Set the type.
+     * 
+     * @param array
+     * 
+     * @return void
+     */
+    public function __construct(array $attributes = [])
+    {
+        $this->setInitialAttributes($attributes);
+    }
+
+    /**
+     * Returns the string representation of the element.
+     * 
+     * @return string
+     */
+    abstract public function __toString() : string;
+
+    /**
+     * Sets the initial attributes.
+     * 
+     * @param array $attributes
+     * 
+     * @return void
+     */
+    protected function setInitialAttributes(array $attributes) : void
+    {
+        $this->setAttributes($attributes);
+    }
+
+    /**
+     * Set the element's attributes.
+     * 
+     * @param array
+     * 
+     * @return Input
+     */
+    public function setAttributes(array $attributes) : Input
+    {
+        $this->attributes = $attributes;
+
+        return $this;
+    }
+
+    /**
+     * Get the element's attributes.
+     * 
+     * @return array
+     */
+    public function getAttributes() : array
+    {
+        return $this->attributes;
+    }
+
+    /**
+     * Add attributes to the element.
+     * 
+     * @param array $attributes
+     * 
+     * @return Input
+     */
+    public function addAttributes(array $attributes) : Input
+    {
+        foreach ($attributes as $attribute_name => $attribute_value) {
+            $this->setAttribute($attribute_name, $attribute_value);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Remove attributes from the element.
+     * 
+     * @param array|null $attributes
+     * 
+     * @return Input
+     */
+    public function removeAttributes(array $attributes = null) : Input
+    {
+        $attributes_to_remove = $attributes ?: $this->getAttributes();
+        
+        foreach ($attributes_to_remove as $attribute_name => $attribute_value) {
+            $this->removeAttribute($attribute_name);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Set an attribute.
+     * 
+     * @param string      $attribute_name
+     * @param string|null $attribute_value
+     * 
+     * @return Input
+     */
+    public function setAttribute(string $attribute_name, string $attribute_value = null) : Input
+    {
+        $this->attributes[$attribute_name] = $attribute_value;
+
+        return $this;
+    }
+
+    /**
+     * Retrieve an attribute's value.
+     * 
+     * @param string $attribute_name
+     * 
+     * @return null|string
+     */
+    public function getAttributeValue(string $attribute_name) : ?string
+    {
+        return $this->attributes[$attribute_name];
+    }
+
+    /**
+     * Removes an attribute from the element.
+     * 
+     * @param string $attribute_name
+     * 
+     * @return Input
+     */
+    public function removeAttribute(string $attribute_name) : Input
+    {
+        unset($this->attributes[$attribute_name]);
+
+        return $this;
+    }
+
+    /**
+     * Returns a boolean indicating whether the element has an attribute.
+     * 
+     * @param string $attribute_name
+     * 
+     * @return bool
+     */
+    public function hasAttribute(string $attribute_name) : bool
+    {
+        return array_key_exists(
+            $attribute_name,
+            $this->getAttributes()
+        );
+    }
+
+    /**
+     * Returns the element's open tag.
+     * 
+     * @return string
+     */
+    public function getOpenTag() : string
+    {
+        $open_tag = '<' . $this->tag_name;
+
+        foreach ($this->getAttributes() as $attribute_name => $attribute_value) {
+            if (is_null($attribute_value)) {
+                $open_tag .= ' ' . $attribute_name;
+            } else {
+                $open_tag .= ' ' . $attribute_name . '="' . $attribute_value . '"';
+            }
+        }
+
+        $open_tag .= '>';
+
+        return $open_tag;
+    }
+
+    /**
+     * Return the element's tag name.
+     * 
+     * @return string
+     */
+    public function getTagName() : string
+    {
+        return $this->tag_name;
+    }
+}
--- src/Input.php
+++ src/Input.php
@@ -6,26 +6,14 @@ namespace Hyacinth;
 
 use Exception;
 
-class Input
+class Input extends DomElement
 {
     /**
-     * An array of attribute name => value pairs for the element.
+     * The tag name of the element.
      * 
-     * @var array
+     * @var string
      */
-    private $attributes = [];
-
-    /**
-     * Instantiate the object. Set the type.
-     * 
-     * @param array
-     * 
-     * @return void
-     */
-    public function __construct(array $attributes = [])
-    {
-        $this->setInitialAttributes($attributes);
-    }
+    protected $tag_name = 'input';
 
     /**
      * Sets the initial attributes. Sets the type to "text" if one was not specified.
@@ -34,75 +22,13 @@ class Input
      * 
      * @return void
      */
-    private function setInitialAttributes(array $attributes) : void
+    protected function setInitialAttributes(array $attributes) : void
     {
-        $this->setAttributes($attributes);
+        parent::setAttributes($attributes);
 
         if (!$this->hasAttribute('type')) {
             $this->setAttribute('type', 'text');
         }
-    }
-
-    /**
-     * Set the element's attributes.
-     * 
-     * @param array
-     * 
-     * @return Input
-     */
-    public function setAttributes(array $attributes) : Input
-    {
-        $this->attributes = $attributes;
-
-        return $this;
-    }
-
-    /**
-     * Add attributes to the element.
-     * 
-     * @param array $attributes
-     * 
-     * @return Input
-     */
-    public function addAttributes(array $attributes) : Input
-    {
-        foreach ($attributes as $attribute_name => $attribute_value) {
-            $this->setAttribute($attribute_name, $attribute_value);
-        }
-
-        return $this;
-    }
-
-    /**
-     * Get the element's attributes.
-     * 
-     * @return array
-     */
-    public function getAttributes() : array
-    {
-        return $this->attributes;
-    }
-
-    /**
-     * Returns the element's open tag.
-     * 
-     * @return string
-     */
-    public function getOpenTag() : string
-    {
-        $open_tag = '<input';
-
-        foreach ($this->getAttributes() as $attribute_name => $attribute_value) {
-            if (is_null($attribute_value)) {
-                $open_tag .= ' ' . $attribute_name;
-            } else {
-                $open_tag .= ' ' . $attribute_name . '="' . $attribute_value . '"';
-            }
-        }
-
-        $open_tag .= '>';
-
-        return $open_tag;
     }
 
     /**
@@ -113,79 +39,5 @@ class Input
     public function __toString() : string
     {
         return $this->getOpenTag();
-    }
-
-    /**
-     * Retrieve an attribute's value.
-     * 
-     * @param string $attribute_name
-     * 
-     * @return null|string
-     */
-    public function getAttributeValue(string $attribute_name) : ?string
-    {
-        return $this->attributes[$attribute_name];
-    }
-
-    /**
-     * Set an attribute.
-     * 
-     * @param string      $attribute_name
-     * @param string|null $attribute_value
-     * 
-     * @return Input
-     */
-    public function setAttribute(string $attribute_name, string $attribute_value = null) : Input
-    {
-        $this->attributes[$attribute_name] = $attribute_value;
-
-        return $this;
-    }
-
-    /**
-     * Removes an attribute from the element.
-     * 
-     * @param string $attribute_name
-     * 
-     * @return Input
-     */
-    public function removeAttribute(string $attribute_name) : Input
-    {
-        unset($this->attributes[$attribute_name]);
-
-        return $this;
-    }
-
-    /**
-     * Returns a boolean indicating whether the element has an attribute.
-     * 
-     * @param string $attribute_name
-     * 
-     * @return bool
-     */
-    public function hasAttribute(string $attribute_name) : bool
-    {
-        return array_key_exists(
-            $attribute_name,
-            $this->getAttributes()
-        );
-    }
-
-    /**
-     * Remove attributes from the element.
-     * 
-     * @param array|null $attributes
-     * 
-     * @return Input
-     */
-    public function removeAttributes(array $attributes = null) : Input
-    {
-        $attributes_to_remove = $attributes ?: $this->getAttributes();
-        
-        foreach ($attributes_to_remove as $attribute_name => $attribute_value) {
-            $this->removeAttribute($attribute_name);
-        }
-
-        return $this;
     }
 }
