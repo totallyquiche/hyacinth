535b1315841ee9e1976de567218be45aabe01123
Brian Dady <bndady@gmail.com>
2020-09-07 06:46:00 +0000

    Creates Div class and tests

--- src/Textarea.php
+++ src/Textarea.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Hyacinth;
 
-use Hyacinth\DomElement;
-
 class Textarea extends StandardElement
 {
     /**
---
+++ tests/unit/DivTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyacinty\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Hyacinth\Div;
+
+class DivTest extends TestCase
+{
+    /**
+     * Test that the element can be instantiated.
+     * 
+     * @return void
+     */
+    public function testCanInstantiate() : void
+    {
+        $this->assertInstanceOf(Div::class, new Div);
+    }
+
+    /**
+     * Test that getTagName() returns expected value.
+     * 
+     * @return void
+     */
+    public function testGetTagName() : void
+    {
+        $this->assertEquals(
+            'div',
+            (new Div)->getTagName()
+        );
+    }
+}
