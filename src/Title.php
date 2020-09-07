a21e7e799041270078b7945dcbc09de097ffb1b4
Brian Dady <bndady@gmail.com>
2020-09-07 15:56:32 +0000

    Adds new Title class and tests

--- src/Title.php
+++ src/Title.php
@@ -5,9 +5,23 @@ declare(strict_types=1);
 namespace Hyacinth;
 
 use Hyacinth\DomElement;
+use Hyacinth\Text;
 
 class Title extends NonVoidElement
 {
+    /**
+     * Initialize the element. Set the Text content.
+     * 
+     * @param array $attributes
+     * @param Text  $text
+     */
+    public function __construct(array $attributes = [], Text $text)
+    {
+        parent::__construct($attributes);
+
+        $this->addContent([$text]);
+    }
+
     /**
      * Return the name of the element.
      * 
--- tests/unit/TitleTest.php
+++ tests/unit/TitleTest.php
@@ -7,6 +7,7 @@ namespace Hyacinty\Tests\Unit;
 use PHPUnit\Framework\TestCase;
 use Hyacinth\NonVoidElement;
 use Hyacinth\Title;
+use Hyacinth\Text;
 
 class TitleTest extends TestCase
 {
@@ -17,7 +18,7 @@ class TitleTest extends TestCase
      */
     public function testCanInstantiate() : void
     {
-        $this->assertInstanceOf(NonVoidElement::class, new Title);
+        $this->assertInstanceOf(NonVoidElement::class, new Title([], new Text('title')));
     }
 
     /**
@@ -29,7 +30,22 @@ class TitleTest extends TestCase
     {
         $this->assertEquals(
             'title',
-            (new Title)->getTagName()
+            (new Title([], new Text('title')))->getTagName()
+        );
+    }
+
+    /**
+     * Test new instance has text in its content.
+     * 
+     * @return void
+     */
+    public function testStringInContent() : void
+    {
+        $text = new Text('text');
+
+        $this->assertEquals(
+            [$text],
+            (new Title([], $text))->getContent()
         );
     }
 }
